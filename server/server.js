bb104a3c3461f14d33a046ae356c3634a4bd0522
Felipe Muhamed <muhamed@cpqd.com.br>
2018-05-23 09:06:59 -0300

    Added GET by ID method

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 //var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,28 @@ app.get('/todos', (req, res) => {
     },(e) => {
         res.status(400).send(e);
     });
+});
+
+//Get /todos/1234324
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    if (!ObjectID.isValid) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then ((todos) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todos});
+
+    }).catch((e) => {
+        return res.status(400).send();
+    });
+
 });
 
 app.listen(3000, () => {
