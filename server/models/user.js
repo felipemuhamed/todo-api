9cf8c94632fe512b7a678e61562b760447d4b3c8
Felipe Muhamed <muhamed@cpqd.com.br>
2018-05-25 09:47:30 -0300

    add Login method

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ var {mongoose} = require('../db/mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs')
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -65,6 +66,45 @@ var UserSchema = new mongoose.Schema({
       '_id': decoded._id,
       'tokens.token': token,
       'tokens.access': 'auth'
+    });
+  };
+
+  UserSchema.pre('save',function(next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          user.password = hash;
+          next();
+        });
+      });
+    }else {
+      next();
+    } 
+  });
+
+  UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+
+      if (!user) {
+        return Promise.reject();
+      }
+
+      return new Promise((resolve, reject) => {
+
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          }else {
+            reject();
+          }
+        });
+
+      });
+
     });
   };
 
--- server/server.js
+++ server/server.js
@@ -113,6 +113,20 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res)=> {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send();
+  });
+
+});
+
 app.listen(3000, () => {
   console.log(`Started up at port 3000`);
 });
